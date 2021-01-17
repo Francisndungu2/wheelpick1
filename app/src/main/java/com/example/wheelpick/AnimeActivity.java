0397b5acea6d700714ed71a278022cbb7ab4961f
Francisndungu2 <francianmbugua@gmail.com>
2021-01-17 18:17:53 +0300

    addanimeactivity

---
+++ app/src/main/java/com/example/wheelpick/AnimeActivity.java
@@ -0,0 +1,14 @@
+package com.example.wheelpick;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+
+public class AnimeActivity extends AppCompatActivity {
+
--- app/src/main/java/com/example/wheelpick/about.java
+++ app/src/main/java/com/example/wheelpick/about.java
@@ -1,40 +1,28 @@
 package com.example.wheelpick;
 
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class about extends AppCompatActivity {
-    ListView listView;
-    TextView textView;
-    String[] listItem;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
 
-        listView=(ListView)findViewById(R.id.listView);
-        textView=(TextView)findViewById(R.id.textView);
-        listItem = getResources().getStringArray(R.array.array_technology);
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, listItem);
-        listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // TODO Auto-generated method stub
-                String value=adapter.getItem(position);
-                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
 
-            }
-        });
+
+
+
     }
-}
+
