b89b2b7af49541129393a9ba6cf9ab9824176369
Francisndungu2 <francianmbugua@gmail.com>
2021-01-17 18:21:20 +0300

    setvalues

--- app/src/main/java/com/example/wheelpick/AnimeActivity.java
+++ app/src/main/java/com/example/wheelpick/AnimeActivity.java
@@ -35,4 +35,20 @@ public class AnimeActivity extends AppCompatActivity {
         CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsingtoolbar_id);
         collapsingToolbarLayout.setTitleEnabled(true);
 
+        TextView tv_name = findViewById(R.id.aa_anime_name);
+        TextView tv_studio = findViewById(R.id.aa_studio);
+        TextView tv_categorie = findViewById(R.id.aa_categorie) ;
+        TextView tv_description = findViewById(R.id.aa_description);
+        TextView tv_rating  = findViewById(R.id.aa_rating) ;
+        ImageView img = findViewById(R.id.aa_thumbnail);
+
+        // setting values to each view
+
+        tv_name.setText(name);
+        tv_categorie.setText(category);
+        tv_description.setText(description);
+        tv_rating.setText(rating);
+        tv_studio.setText(studio);
+
+
 }
