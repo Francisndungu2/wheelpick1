509d6267a492435bc1e0d8077c0c2c640d0ceffa
Francisndungu2 <francianmbugua@gmail.com>
2021-01-17 18:20:20 +0300

    adddatareceiver

--- app/src/main/java/com/example/wheelpick/AnimeActivity.java
+++ app/src/main/java/com/example/wheelpick/AnimeActivity.java
@@ -12,3 +12,27 @@ import com.bumptech.glide.request.RequestOptions;
 
 public class AnimeActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anime);
+
+        // hide the default actionbar
+        getSupportActionBar().hide();
+
+        // Recieve data
+
+        String name  = getIntent().getExtras().getString("anime_name");
+        String description = getIntent().getExtras().getString("anime_description");
+        String studio = getIntent().getExtras().getString("anime_studio") ;
+        String category = getIntent().getExtras().getString("anime_category");
+        int nb_episode = getIntent().getExtras().getInt("anime_nb_episode") ;
+        String rating = getIntent().getExtras().getString("anime_rating") ;
+        String image_url = getIntent().getExtras().getString("anime_img") ;
+
+        // ini views
+
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsingtoolbar_id);
+        collapsingToolbarLayout.setTitleEnabled(true);
+
+}
